eb8049f58efeee6e1ee0449a22379e1223c1d76c
Mehek <shahmehek176@gmail.com>
2025-08-05 22:24:48 +0530

    Update urls.py

--- bartender/cocktails/urls.py
+++ bartender/cocktails/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import search_cocktails,cocktail_detail
+from .views import search_cocktails,cocktail_detail,database_display
 
 urlpatterns = [
     path('',search_cocktails, name='search'),
     path('cocktail/<int:drink_id>/',cocktail_detail, name='cocktail_detail'),
+    path('cocktail/popular_cocktails/', database_display, name='display'),
 ]
 
 
